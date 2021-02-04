915b6296f1f3634da8031fb1ac406ea098fe60d0
zanderson1998 <zanderson1998@yahoo.com>
2021-02-04 12:54:52 -0700

    add delete and party request routes

--- server.js
+++ server.js
@@ -108,6 +108,54 @@ app.get('/api/candidates', (req, res) => {
     });
 });
 
+//get all parties
+app.get('/api/parties', (req,res) => {
+    const sql = `SELECT * FROM parties`;
+    const params = [];
+    db.all(sql, params, (err, rows) => {
+        if (err) {
+            res.status(500).json({ error: err.message });
+            return;
+        }
+
+        res.json({
+            message: 'success',
+            data: rows
+        });
+    });
+});
+
+//get route for single party id
+app.get('/api/party/:id', (req, res) => {
+    const sql =  `SELECT * FROM parties WHERE id = ?`;
+    const params = [req.params.id];
+    db.get(sql, params, (err, row) => {
+        if (err) {
+            res.status(400).json({ error: err.message });
+            return;
+        }
+
+        res.json({
+            message: 'success',
+            data: row
+        });
+    });
+});
+
+//add delete party route
+app.delete('/api/party/:id', (req, res) => {
+    const sql = `DELETE FROM parties WHERE id = ?`;
+    const params = [req.params.id];
+    db.run(sql, params, function(err, result) {
+        if (err) {
+            res.status(400).json({ error: res.message });
+            return;
+        }
+
+        res.json({ message: 'successfully deleted', changes: this.changes });
+    });
+});
+
 //default response for any other request (not found) catach all
 app.use((req, res) => {
     res.status(404).end();
