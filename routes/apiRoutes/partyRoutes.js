79c09e53f653c323b7be328f36bba2a94c7ad854
zanderson1998 <zanderson1998@yahoo.com>
2021-02-04 16:21:21 -0700

    add routes for voters and modularize code

---
+++ routes/apiRoutes/partyRoutes.js
@@ -0,0 +1,54 @@
+const express = require('express');
+const router = express.Router();
+const db = require('../../db/database');
+
+
+//get all parties
+router.get('/parties', (req,res) => {
+    const sql = `SELECT * FROM parties`;
+    const params = [];
+    db.all(sql, params, (err, rows) => {
+        if (err) {
+            res.status(500).json({ error: err.message });
+            return;
+        }
+
+        res.json({
+            message: 'success',
+            data: rows
+        });
+    });
+});
+
+//get route for single party id
+router.get('/party/:id', (req, res) => {
+    const sql =  `SELECT * FROM parties WHERE id = ?`;
+    const params = [req.params.id];
+    db.get(sql, params, (err, row) => {
+        if (err) {
+            res.status(400).json({ error: err.message });
+            return;
+        }
+
+        res.json({
+            message: 'success',
+            data: row
+        });
+    });
+});
+
+//add delete party route
+router.delete('/party/:id', (req, res) => {
+    const sql = `DELETE FROM parties WHERE id = ?`;
+    const params = [req.params.id];
+    db.run(sql, params, function(err, result) {
+        if (err) {
+            res.status(400).json({ error: res.message });
+            return;
+        }
+
+        res.json({ message: 'successfully deleted', changes: this.changes });
+    });
+});
+
+module.exports = router;
